8ab0a98aeccd9ac7f17cea96552a4c0ed56c0a8e
fajarpratamap <fajarpratamap@gmail.com>
2025-10-21 16:49:36 +0700

    create file go in root

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"quizy/internal/quiz"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	dsn, err := quiz.DSNFromEnv()
+	if err != nil {
+		log.Fatalf("config error: %v", err)
+	}
+
+	svc, err := quiz.NewService(dsn)
+	if err != nil {
+		log.Fatalf("service init: %v", err)
+	}
+	defer svc.Close()
+
+	mux := http.NewServeMux()
+	mux.Handle("/api/quiz", svc)
+
+	addr := defaultAddr()
+	log.Printf("listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func defaultAddr() string {
+	if v := os.Getenv("PORT"); v != "" {
+		return ":" + v
+	}
+	return ":8080"
+}
