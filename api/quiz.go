75d1b7f8ae7c24f8e54b3623c7487c6dde60a3ef
fajarpratamap <fajarpratamap@gmail.com>
2025-10-22 08:06:10 +0700

    fix bug service

--- api/quiz.go
+++ api/quiz.go
@@ -9,27 +9,40 @@ import (
 )
 
 var (
-	once sync.Once
-	svc  *quiz.Service
-	err  error
+	svc   *quiz.Service
+	svcMu sync.Mutex
 )
+
+func getService() (*quiz.Service, error) {
+	svcMu.Lock()
+	defer svcMu.Unlock()
+
+	if svc != nil {
+		return svc, nil
+	}
+
+	dsn, err := quiz.DSNFromEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	service, err := quiz.NewService(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	svc = service
+	return svc, nil
+}
 
 // Handler is the Vercel entrypoint.
 func Handler(w http.ResponseWriter, r *http.Request) {
-	once.Do(func() {
-		var dsn string
-		dsn, err = quiz.DSNFromEnv()
-		if err != nil {
-			return
-		}
-		svc, err = quiz.NewService(dsn)
-	})
-
+	service, err := getService()
 	if err != nil {
 		http.Error(w, `{"error":"service initialization failed"}`, http.StatusInternalServerError)
 		log.Printf("handler init: %v", err)
 		return
 	}
 
-	svc.ServeHTTP(w, r)
+	service.ServeHTTP(w, r)
 }
